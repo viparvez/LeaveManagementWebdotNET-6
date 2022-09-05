b3935b0b4bcb5d0e0a91b144c3160ebe03c2749d
Sirajum Monir Parvez <viparvez@gmail.com>
2022-09-05 22:53:06 +0600

    Added LeaveTypes and LeaveAllocations Table

---
+++ LeaveManagementWeb/Data/LeaveAllocation.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementWeb.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int NumberOfDays { get; set; }
+        public string EmployeeId { get; set; }
+       
+    }
+}
---
+++ LeaveManagementWeb/Data/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagementWeb.Data
+{
+    public class LeaveType : BaseEntity
+    {
+        public string Name { get; set; }
+        //DefaultDays; Maximum allowed leaves per type, e.g., Sick Leave - (15 days), Annual Leave (20 days)
+        public int DefaultDays { get; set; }
+    }
+}
